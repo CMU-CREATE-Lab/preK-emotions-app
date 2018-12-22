01623a5d37ad46551862b12fbb39a9c45a74d6a5
mtasota <miketasota@gmail.com>
2018-12-22 16:09:15 -0500

    Added side panel and fixed clipping issues (requires minSdkVersion=21)

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/StudentIndexAdapter.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/StudentIndexAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.cmucreatelab.android.flutterprek.R;
@@ -40,11 +41,15 @@ public class StudentIndexAdapter extends BaseAdapter {
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             result = LayoutInflater.from(parent.getContext()).inflate(R.layout.student_grid_view_item, parent, false);
+            // NOTE: requires api level 21
+            result.findViewById(R.id.imageView).setClipToOutline(true);
         } else {
             result = convertView;
         }
         TextView textView = (TextView)result.findViewById(R.id.text1);
         textView.setText(students.get(position).getName());
+        // TODO replace with student image
+        ((ImageView)result.findViewById(R.id.imageView)).setImageResource(R.mipmap.ic_launcher);
 
         return result;
     }
