d41cdc861012b78d4528ed3eb9184f64c4fa4c83
mtasota <miketasota@gmail.com>
2018-12-22 14:27:18 -0500

    pass classroom instead of string

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/ClassroomsIndexActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/ClassroomsIndexActivity.java
@@ -7,20 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import org.cmucreatelab.android.flutterprek.R;
 import org.cmucreatelab.android.flutterprek.database.AppDatabase;
 import org.cmucreatelab.android.flutterprek.database.models.classroom.Classroom;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ClassroomsIndexActivity extends AppCompatActivity {
 
-
-    final ArrayList<String> myStringArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +26,8 @@ public class ClassroomsIndexActivity extends AppCompatActivity {
         AppDatabase.getInstance(this).classroomDAO().getAllClassrooms().observe(this, new Observer<List<Classroom>>() {
             @Override
             public void onChanged(@Nullable List<Classroom> classrooms) {
-                ArrayList<String> classroomNames = new ArrayList<>();
-                for (Classroom c: classrooms) {
-                    classroomNames.add(c.getName());
-                }
-                classroomNames.add("Foo");
-                classroomNames.add("Bar");
-                myStringArray.clear();
-                myStringArray.addAll(classroomNames);
                 GridView classroomsGridView = findViewById(R.id.classroomsGridView);
-//                classroomsGridView.setAdapter(new ArrayAdapter<>(ClassroomsIndexActivity.this,
-//                        android.R.layout., myStringArray));
-                classroomsGridView.setAdapter(new ClassroomIndexAdapter(myStringArray));
+                classroomsGridView.setAdapter(new ClassroomIndexAdapter(classrooms));
             }
         });
 
