e1c27237b0a7f9dd3f87a1a6197c4bb75c795ad9
mtasota <miketasota@gmail.com>
2018-12-25 16:05:17 -0500

    Add click listeners on icons in header

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/fragments/AppHeaderFragment.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/fragments/AppHeaderFragment.java
@@ -2,17 +2,23 @@ package org.cmucreatelab.android.flutterprek.activities.fragments;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.cmucreatelab.android.flutterprek.Constants;
 import org.cmucreatelab.android.flutterprek.R;
 
 public class AppHeaderFragment extends Fragment {
 
 
     /** Get the resource ("R.layout." ...) for the extended Fragment class. */
+    @LayoutRes
     public int getInflatedLayoutResource() {
         return R.layout.fragment_app_header;
     }
@@ -24,8 +30,30 @@ public class AppHeaderFragment extends Fragment {
     }
 
 
-    public void setListeners() {
-        // TODO button listeners for header
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // TODO actions when icons clicked
+
+        view.findViewById(R.id.imageLogo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(Constants.LOG_TAG, "imageLogo.onClick");
+            }
+        });
+        view.findViewById(R.id.imageStudent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(Constants.LOG_TAG, "imageStudent.onClick");
+            }
+        });
+        view.findViewById(R.id.imageInfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(Constants.LOG_TAG, "imageInfo.onClick");
+            }
+        });
     }
 
 
