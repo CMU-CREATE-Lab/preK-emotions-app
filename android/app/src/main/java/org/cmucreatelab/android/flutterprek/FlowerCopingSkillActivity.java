f9de263cb709295d7a60aa1477ea77158e175470
mtasota <miketasota@gmail.com>
2018-12-27 18:30:56 -0500

    Added ladybug and hand images

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/FlowerCopingSkillActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/FlowerCopingSkillActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import org.cmucreatelab.android.flutterprek.bluetooth_birdbrain.UARTConnection;
 
@@ -53,6 +54,16 @@ public class FlowerCopingSkillActivity extends AppCompatActivity {
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // TODO need to detect when nav bar is forced to display, so it can hide again after a few seconds
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
 
