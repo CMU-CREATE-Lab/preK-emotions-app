c52d016d3c7a9e899c95b50a551ab5b8fcf238e8
mtasota <miketasota@gmail.com>
2019-01-09 15:44:32 -0500

    use immersive sticky instead of fullscreen (thank you parv)

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/coping_skill_flower/FlowerCopingSkillActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/coping_skill_flower/FlowerCopingSkillActivity.java
@@ -62,14 +62,10 @@ public class FlowerCopingSkillActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        // TODO need to detect when nav bar is forced to display, so it can hide again after a few seconds
         View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
         decorView.setSystemUiVisibility(uiOptions);
 
         flowerStateHandler.lookForFlower();
