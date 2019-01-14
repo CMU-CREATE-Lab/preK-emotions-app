ffad03c6e91d833f8068360c267a28ef621bee80
mtasota <miketasota@gmail.com>
2019-01-13 21:34:03 -0500

    long click listener, placeholder for eventual admin dialog

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/StudentSectionActivityWithHeader.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/StudentSectionActivityWithHeader.java
@@ -1,7 +1,10 @@
 package org.cmucreatelab.android.flutterprek.activities.student_section;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import org.cmucreatelab.android.flutterprek.Constants;
 import org.cmucreatelab.android.flutterprek.R;
 import org.cmucreatelab.android.flutterprek.activities.AbstractActivity;
 import org.cmucreatelab.android.flutterprek.activities.fragments.AppHeaderFragment;
@@ -11,11 +14,26 @@ public abstract class StudentSectionActivityWithHeader extends AbstractActivity 
     private AppHeaderFragment headerFragment;
 
 
+    private void showAdminDialog() {
+        // TODO https://developer.android.com/guide/topics/ui/dialogs
+        // also, try and make this conform to the design (will serve as a good template)
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.headerFragment = (AppHeaderFragment) (getSupportFragmentManager().findFragmentById(R.id.appHeader));
         this.headerFragment.setHeaderTransparency(true);
+
+        findViewById(R.id.imageLogo).setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.i(Constants.LOG_TAG, "long clicked the MindfulNest Logo.");
+                showAdminDialog();
+                return true;
+            }
+        });
     }
 
 }
