5d38ea2fba1af1a5fe5629be74726e12d0e009b8
mtasota <miketasota@gmail.com>
2019-01-13 20:58:49 -0500

    avoid playing audio when app is in the background

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/AbstractActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/AbstractActivity.java
@@ -3,6 +3,10 @@ package org.cmucreatelab.android.flutterprek.activities;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.cmucreatelab.android.flutterprek.AudioPlayer;
+import org.cmucreatelab.android.flutterprek.Constants;
 
 /**
  * All activities in the project should extend from this class.
@@ -14,6 +18,15 @@ public abstract class AbstractActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getResourceIdForActivityLayout());
+    }
+
+
+    /** Makes sure the audio player is stopped before the app goes in the background. */
+    @Override
+    protected void onPause() {
+        Log.i(Constants.LOG_TAG, "onPause");
+        AudioPlayer.getInstance(getApplicationContext()).stop();
+        super.onPause();
     }
 
 
