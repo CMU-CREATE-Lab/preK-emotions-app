c95a9f739a91177bfc160f1018caac680e27d992
mtasota <miketasota@gmail.com>
2019-01-20 18:43:48 -0500

    added 'rejoin friends' navigation but still need to implement recording and proper info between activities

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/post_coping_skills/post_coping_skill_rejoin_friends/RejoinFriendsActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/post_coping_skills/post_coping_skill_rejoin_friends/RejoinFriendsActivity.java
@@ -1,9 +1,22 @@
 package org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.post_coping_skills.post_coping_skill_rejoin_friends;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import org.cmucreatelab.android.flutterprek.R;
+import org.cmucreatelab.android.flutterprek.activities.student_section.ChooseCopingSkillActivity;
+import org.cmucreatelab.android.flutterprek.activities.student_section.ChooseStudentActivity;
 import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.post_coping_skills.PostCopingSkillActivity;
 
 public class RejoinFriendsActivity extends PostCopingSkillActivity {
+
+
+    private void goToNextPostCopingSkillActivity(Class nextClass) {
+        Intent intent = new Intent(this, nextClass);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 
 
     @Override
@@ -17,5 +30,24 @@ public class RejoinFriendsActivity extends PostCopingSkillActivity {
         return R.layout._coping_skill__activity_rejoin_friends;
     }
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        findViewById(R.id.imageViewNo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO use "choose something else" instead of the regular Choose coping skill
+                goToNextPostCopingSkillActivity(ChooseCopingSkillActivity.class);
+            }
+        });
+        findViewById(R.id.imageViewYes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToNextPostCopingSkillActivity(ChooseStudentActivity.class);
+            }
+        });
+    }
 
 }
