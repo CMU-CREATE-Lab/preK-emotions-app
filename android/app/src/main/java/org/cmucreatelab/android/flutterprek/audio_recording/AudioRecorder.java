0c99c9b5c9642ed803b97cdd2cf9234704975006
mtasota <miketasota@gmail.com>
2019-01-21 10:36:45 -0500

    copied files from mfm for audio recording

---
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/audio_recording/AudioRecorder.java
@@ -0,0 +1,73 @@
+package org.cmucreatelab.android.flutterprek.audio_recording;
+
+import android.content.Context;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import org.cmucreatelab.android.flutterprek.Constants;
+import org.cmucreatelab.android.flutterprek.GlobalHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by Steve on 5/17/2016.
+ * Modified by tasota on 1/21/2019.
+ *
+ * AudioRecorder
+ *
+ * A helper class to simplify recording audio.
+ *
+ */
+public class AudioRecorder implements Serializable {
+
+    private Context appContext;
+    private MediaRecorder mediaRecorder;
+    private File audioFile;
+    public boolean isRecording;
+
+
+    // class methods
+
+
+    public void startRecording() {
+        //GlobalHandler globalHandler = GlobalHandler.getInstance(appContext);
+        //this.audioFile = SaveFileHandler.getOutputMediaFile(appContext, SaveFileHandler.MEDIA_TYPE_AUDIO, globalHandler);
+        this.audioFile = SaveFileHandler.getOutputMediaFile(appContext, SaveFileHandler.MEDIA_TYPE_AUDIO, GlobalHandler.getInstance(appContext));
+        this.mediaRecorder = new MediaRecorder();
+        this.mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        this.mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+        this.mediaRecorder.setOutputFile(audioFile.getAbsolutePath());
+        this.mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        //globalHandler.sessionHandler.setMessageAudio(this.audioFile);
+        // TODO need access to created file
+
+        try {
+            this.mediaRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(Constants.LOG_TAG, "Could not prepare Recorder.");
+        }
+
+        this.mediaRecorder.start();
+        this.isRecording = true;
+    }
+
+
+    public void stopRecording() {
+        if (mediaRecorder != null && isRecording) {
+            this.mediaRecorder.stop();
+            this.mediaRecorder.release();
+            this.mediaRecorder = null;
+            this.isRecording = false;
+        }
+    }
+
+
+    public AudioRecorder(Context appContext) {
+        this.appContext = appContext;
+        this.isRecording = false;
+        this.mediaRecorder = null;
+    }
+
+}
---
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/audio_recording/SaveFileHandler.java
@@ -0,0 +1,67 @@
+package org.cmucreatelab.android.flutterprek.audio_recording;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import org.cmucreatelab.android.flutterprek.GlobalHandler;
+
+import java.io.File;
+
+/**
+ * Created by Steve on 3/18/2016.
+ * Modified by tasota on 1/21/2019.
+ *
+ * SaveFileHandler
+ *
+ * Used to help create and save files of various types.
+ */
+public class SaveFileHandler {
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_AUDIO = 2;
+
+
+    public static File getOutputMediaFile(Context context, int type, GlobalHandler globalHandler) {
+        File mediaStorageDir = new File("","");
+        File mediaFile;
+
+
+        //String name = globalHandler.sessionHandler.getMessageSender().getName();
+        String name = globalHandler.studentSectionNavigationHandler.studentUuid;
+        if (name.contains(" ")) {
+            name = name.replace(' ', '_');
+        }
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaStorageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), name);
+        } else if (type == MEDIA_TYPE_AUDIO) {
+            mediaStorageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_MUSIC), name);
+        }
+
+        if (! mediaStorageDir.exists()) {
+            if (! mediaStorageDir.mkdirs()) {
+                Log.d("Message From Me", "failed to create directory");
+                return null;
+            }
+        }
+
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + name + ".jpeg");
+            mediaFile.delete();
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + name + ".jpeg");
+        } else if (type == MEDIA_TYPE_AUDIO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "AUDIO_" + name + ".amr-nb");
+            mediaFile.delete();
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "AUDIO_" + name + ".amr-nb");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+}
