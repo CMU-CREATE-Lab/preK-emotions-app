b68b072e3a59a67ad8471f016f2008f23015f8c0
mtasota <miketasota@gmail.com>
2019-01-21 17:05:05 -0500

    Hardcoded check for displaying emotion on post coping skill activity

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/post_coping_skills/post_coping_skill_finished_exercise/FinishedExerciseActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/post_coping_skills/post_coping_skill_finished_exercise/FinishedExerciseActivity.java
@@ -3,7 +3,10 @@ package org.cmucreatelab.android.flutterprek.activities.student_section.coping_s
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.cmucreatelab.android.flutterprek.GlobalHandler;
 import org.cmucreatelab.android.flutterprek.R;
 import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.post_coping_skills.PostCopingSkillActivity;
 import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.post_coping_skills.post_coping_skill_use_words.UseWordsActivity;
@@ -14,6 +17,30 @@ public class FinishedExerciseActivity extends PostCopingSkillActivity {
     private void goToNextPostCopingSkillActivity() {
         Intent intent = new Intent(this, UseWordsActivity.class);
         startActivity(intent);
+    }
+
+
+    // TODO hardcoded for now but should be defined elsewhere in the future
+    private void determineDisplayForFeelingUnchanged() {
+        String emotionUuid = GlobalHandler.getInstance(getApplicationContext()).studentSectionNavigationHandler.emotionUuid;
+        int imageResource;
+        String title;
+
+        if (emotionUuid.equals("emotion2")) {
+            // sad
+            imageResource = R.drawable.ic_sad;
+            title = "Sad";
+        } else if (emotionUuid.equals("emotion3")) {
+            // mad
+            imageResource = R.drawable.ic_mad;
+            title = "Mad";
+        } else {
+            // hide by default (happy)
+            findViewById(R.id.viewFeelingUnchanged).setVisibility(View.GONE);
+            return;
+        }
+        ((ImageView)findViewById(R.id.imageView1)).setImageResource(imageResource);
+        ((TextView)findViewById(R.id.text1)).setText(title);
     }
 
 
@@ -32,6 +59,8 @@ public class FinishedExerciseActivity extends PostCopingSkillActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        determineDisplayForFeelingUnchanged();
 
         findViewById(R.id.viewFeelingUnchanged).setOnClickListener(new View.OnClickListener() {
             @Override
