07c09e4661c20929437b4d6776fc752170eb6639
Kayla <kaylamyhome@gmail.com>
2019-02-18 15:26:05 -0500

    Added JumpingJacks Video

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/coping_skill_static/JumpingJacksCopingSkillActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/coping_skill_static/JumpingJacksCopingSkillActivity.java
@@ -2,7 +2,13 @@ package org.cmucreatelab.android.flutterprek.activities.student_section.coping_s
 
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
+import android.support.constraint.ConstraintLayout;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.cmucreatelab.android.flutterprek.R;
 import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.AbstractCopingSkillActivity;
@@ -15,11 +21,20 @@ public class JumpingJacksCopingSkillActivity extends AbstractCopingSkillActivity
         super.onCreate(savedInstanceState);
 
         findViewById(R.id.activityBackground).setBackgroundResource(getResourceForBackground());
+        findViewById(R.id.overlayYesNo).setVisibility(View.GONE);
         TextView textViewTitle = findViewById(R.id.textViewTitle);
         textViewTitle.setText(getTextTitleResource());
         textViewTitle.setTextColor(getColorResourceForTitle());
-    }
 
+        ConstraintLayout backgroundView = findViewById(R.id.activityBackground);
+
+        VideoView videoView = new VideoView(this);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.jumpingjacks);
+
+        backgroundView.addView(videoView, new ViewGroup.LayoutParams(400, 400));
+
+        videoView.start();
+    }
 
     @Override
     protected void onPause() {
