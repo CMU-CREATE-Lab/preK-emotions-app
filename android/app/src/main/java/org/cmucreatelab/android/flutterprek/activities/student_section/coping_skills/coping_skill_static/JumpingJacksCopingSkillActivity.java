ebb8931729cb985bf7179c66dd8d6303a80d674e
Kayla <kaylamyhome@gmail.com>
2019-03-04 11:47:11 -0500

    VideoPlayer Added

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/coping_skill_static/JumpingJacksCopingSkillActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/coping_skill_static/JumpingJacksCopingSkillActivity.java
@@ -1,18 +1,12 @@
 package org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.coping_skill_static;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.view.View;
-import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 import org.cmucreatelab.android.flutterprek.R;
 import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.AbstractCopingSkillActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
 
 import static org.cmucreatelab.android.flutterprek.R.id.textViewTitle;
 
@@ -28,7 +22,6 @@ public class JumpingJacksCopingSkillActivity extends AbstractCopingSkillActivity
         findViewById(R.id.overlayYesNo).setVisibility(View.GONE);
 
         videoView = findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.jumpingjacks);
 
         TextView textViewTitle = findViewById(R.id.textViewTitle);
         textViewTitle.setText(getTextTitleResource());
@@ -46,40 +39,6 @@ public class JumpingJacksCopingSkillActivity extends AbstractCopingSkillActivity
     protected void onResume() {
         super.onResume();
         playAudio(getAudioFileForCopingSkillTitle());
-
-
-        MediaController mc = new MediaController(this);
-        mc.setAnchorView(videoView);
-        mc.setMediaPlayer(videoView);
-        videoView.setMediaController(mc);
-
-        final Timer pauseVideo = new Timer();
-        final Timer resumeVideo = new Timer();
-
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-
-            @Override
-            public final void onPrepared(final MediaPlayer mp) {
-                mp.setVolume(0f, 0f);
-                mp.start();
-
-                pauseVideo.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        mp.pause();
-
-                        resumeVideo.schedule(new TimerTask() {
-                            @Override
-                            public void run() {
-                                mp.start();
-                            }
-                        }, 1000);
-                    }
-                }, 5000);
-
-
-            }
-        });
 
     }
 
---
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/video/VideoPlayer.java
@@ -0,0 +1,78 @@
+package org.cmucreatelab.android.flutterprek.video;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.widget.MediaController;
+import android.widget.VideoView;
+import org.cmucreatelab.android.flutterprek.R;
+import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.AbstractCopingSkillActivity;
+
+
+public class VideoPlayer {
+
+    private static VideoPlayer classInstance;
+
+    private static VideoView appVideoView;
+    private Context appContext;
+
+    private VideoPlayer(Context context) {
+        appContext = context;
+    }
+
+    public static VideoPlayer getInstance(Context context) {
+        if (classInstance == null) {
+            classInstance = new VideoPlayer(context);
+        }
+        return classInstance;
+    }
+
+    public void Init(Activity activity, VideoView videoView, String videoPath) {
+        appVideoView = videoView;
+        videoView.setVideoPath(videoPath);
+        MediaController mc = new MediaController(activity);
+        mc.setAnchorView(videoView);
+        mc.setMediaPlayer(videoView);
+        videoView.setMediaController(mc);
+    }
+
+    public void playVideo() {
+        initializePlayer();
+    }
+
+    public void pause() {
+        appVideoView.pause();
+    }
+
+    public void stop() {
+        releasePlayer();
+    }
+
+    private void initializePlayer() {
+
+        appVideoView.setOnPreparedListener(
+                new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        // Start playing!
+                        mediaPlayer.setVolume(0f, 0f);
+                        appVideoView.start();
+                    }
+                });
+
+        // Listener for onCompletion() event (runs after media has finished playing).
+        appVideoView.setOnCompletionListener(
+                new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        // Return the video position to the start.
+                        appVideoView.seekTo(0);
+                    }
+                });
+    }
+
+    private void releasePlayer() {
+        appVideoView.stopPlayback();
+    }
+
+}
