d2bd0309a08b380cc7dba06e4a0491220917dbf3
mtasota <miketasota@gmail.com>
2019-03-29 15:14:55 -0400

    Added layout for talk to teacher skill (resolves #69)

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/coping_skill_talk_to_the_teacher/TalkToTheTeacherCopingSkillActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/coping_skill_talk_to_the_teacher/TalkToTheTeacherCopingSkillActivity.java
@@ -1,5 +1,7 @@
 package org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.coping_skill_talk_to_the_teacher;
 
+import android.support.annotation.ColorRes;
+
 import org.cmucreatelab.android.flutterprek.R;
 import org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.coping_skill_static.StaticCopingSkillActivity;
 
@@ -29,6 +31,19 @@ public class TalkToTheTeacherCopingSkillActivity extends StaticCopingSkillActivi
     @Override
     public long getMillisecondsToDisplayOverlay() {
         return DISPLAY_OVERLAY_AFTER_MILLISECONDS;
+    }
+
+
+    @Override
+    public int getResourceIdForActivityLayout() {
+        return R.layout.activity_static_coping_skill_alternative;
+    }
+
+
+    @Override
+    @ColorRes
+    public int getColorResourceForTitle() {
+        return R.color.colorBlack;
     }
 
 }
