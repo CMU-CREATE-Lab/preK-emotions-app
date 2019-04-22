be02b9d0ffb921b569d9f301f09aaf4844b0b370
Kayla <kaylamyhome@gmail.com>
2019-04-22 10:52:04 -0400

    In Progress FeelHeartFragment

---
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/student_section/coping_skills/post_coping_skills/post_coping_skill_heart_beating/fragments/VideoFragment.java
@@ -0,0 +1,4 @@
+package org.cmucreatelab.android.flutterprek.activities.student_section.coping_skills.post_coping_skills.post_coping_skill_heart_beating.fragments;
+
+public class VideoFragment {
+}
