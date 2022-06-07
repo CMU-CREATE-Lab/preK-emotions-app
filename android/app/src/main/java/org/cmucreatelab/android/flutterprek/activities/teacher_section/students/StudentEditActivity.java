6f6764ab849a7e004c75b53da3408f2c3d15a2ee
mtasota <miketasota@gmail.com>
2022-06-07 11:59:57 -0400

    StudentEditActivity: populate views/text, scale image in view

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/teacher_section/students/StudentEditActivity.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/teacher_section/students/StudentEditActivity.java
@@ -1,21 +1,31 @@
 package org.cmucreatelab.android.flutterprek.activities.teacher_section.students;
 
+import android.arch.lifecycle.Observer;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import org.cmucreatelab.android.flutterprek.Constants;
 import org.cmucreatelab.android.flutterprek.R;
+import org.cmucreatelab.android.flutterprek.Util;
 import org.cmucreatelab.android.flutterprek.activities.AbstractActivity;
 import org.cmucreatelab.android.flutterprek.activities.fragments.StudentEditFragment;
+import org.cmucreatelab.android.flutterprek.database.AppDatabase;
+import org.cmucreatelab.android.flutterprek.database.models.db_file.DbFile;
 import org.cmucreatelab.android.flutterprek.database.models.student.Student;
 
 public class StudentEditActivity extends AbstractActivity {
 
     public static final String EXTRA_CLASSROOM_NAME = "classroom_name";
     public static final String EXTRA_STUDENT = "student";
+
+    private static final String headerTitleAddStudent = "Add Student";
+    private static final String headerTitleEditStudent = "Edit Student";
 
     // models/objects
     private String classroomName;
@@ -24,6 +34,31 @@ public class StudentEditActivity extends AbstractActivity {
     // views
     private StudentEditFragment headerFragment;
     private ImageButton imageButtonStudentPhoto;
+
+
+    private void populateViews() {
+        // update header text
+        headerFragment.textViewBack.setText((classroomName != null && !classroomName.isEmpty()) ? String.format("Back to %s", classroomName) : "Back");
+        headerFragment.textViewTitle.setText(headerTitleEditStudent);
+
+        // update views with student info
+        ((EditText)findViewById(R.id.editTextStudentName)).setText(student.getName());
+        ((EditText)findViewById(R.id.editTextStudentNotes)).setText(student.getNotes());
+
+        // get image
+        if (student.getPictureFileUuid() != null) {
+            final Context appContext = getApplicationContext();
+            AppDatabase.getInstance(appContext).dbFileDAO().getDbFile(student.getPictureFileUuid()).observe(this, new Observer<DbFile>() {
+                @Override
+                public void onChanged(@Nullable DbFile dbFile) {
+                    // TODO check if file type is asset
+                    Util.setImageViewWithAsset(appContext, (ImageButton) findViewById(R.id.imageButtonStudentPhoto), dbFile.getFilePath());
+                }
+            });
+        } else {
+            ((ImageButton) findViewById(R.id.imageButtonStudentPhoto)).setImageResource(R.drawable.ic_placeholder);
+        }
+    }
 
 
     @Override
@@ -40,8 +75,7 @@ public class StudentEditActivity extends AbstractActivity {
         this.headerFragment.setHeaderTransparency(true);
         this.imageButtonStudentPhoto = findViewById(R.id.imageButtonStudentPhoto);
 
-        // update header text
-        headerFragment.textViewBack.setText((classroomName != null && !classroomName.isEmpty()) ? String.format("Back to %s", classroomName) : "Back");
+        populateViews();
 
         // avoid EditText focus when activity starts: https://stackoverflow.com/questions/4149415/onscreen-keyboard-opens-automatically-when-activity-starts
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
