4866e35f4ddc75cbf2f3ee7f150bcf34daff95ad
mtasota <miketasota@gmail.com>
2025-06-23 18:54:39 -0400

    highlights design: replace fragments with include (later as custom views)

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/teacher_section/highlights_design/HighlightsDesignActivityWithHeaderAndDrawer.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/activities/teacher_section/highlights_design/HighlightsDesignActivityWithHeaderAndDrawer.java
@@ -1,16 +1,35 @@
 package org.cmucreatelab.android.flutterprek.activities.teacher_section.highlights_design;
 
-import org.cmucreatelab.android.flutterprek.activities.fragments.DrawerTeacherMainFragment;
-import org.cmucreatelab.android.flutterprek.activities.teacher_section.TeacherSectionActivityWithHeaderAndDrawer;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
-public abstract class HighlightsDesignActivityWithHeaderAndDrawer extends TeacherSectionActivityWithHeaderAndDrawer {
+import org.cmucreatelab.android.flutterprek.R;
+import org.cmucreatelab.android.flutterprek.activities.AbstractActivity;
+import org.cmucreatelab.android.flutterprek.activities.teacher_section.ActiveClassroomIndexActivity;
+import org.cmucreatelab.android.flutterprek.activities.teacher_section.classrooms.ClassroomIndexActivity;
+
+public abstract class HighlightsDesignActivityWithHeaderAndDrawer extends AbstractActivity {
 
     // TODO Header, Drawer
+    private View appHeaderHighlights, drawerHighlights;
 
-    // TODO replace Drawer object
     @Override
-    public DrawerTeacherMainFragment.Section getSectionForDrawer() {
-        return null;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.appHeaderHighlights = findViewById(R.id.appHeaderHighlights);
+        this.drawerHighlights = findViewById(R.id.drawerHighlights);
+
+        // TODO remove later (provided for backward navigation only)
+        drawerHighlights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent classroomsIndexActivity = new Intent(getApplicationContext(), ClassroomIndexActivity.class);
+                classroomsIndexActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(classroomsIndexActivity);
+            }
+        });
     }
 
 }
