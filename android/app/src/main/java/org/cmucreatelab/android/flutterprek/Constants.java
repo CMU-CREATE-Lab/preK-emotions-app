21dfb6bd11c79c35983132aed1fc296e768386d9
Dante <dantejp@umich.edu>
2025-07-30 13:58:24 -0400

    changed db_seed

--- android/app/src/main/java/org/cmucreatelab/android/flutterprek/Constants.java
+++ android/app/src/main/java/org/cmucreatelab/android/flutterprek/Constants.java
@@ -90,7 +90,7 @@ public class Constants {
 
 
     /** This file is defined in the assets/ directory. */
-    public static final String DATABASE_SEED = "DbSeed_Spring2024.json";
+    public static final String DATABASE_SEED = "full_db_seed_fixed_updated.json";
 
 }
 
